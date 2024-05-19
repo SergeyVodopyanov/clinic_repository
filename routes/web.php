766a06aa741853f892bfba50ced8c1269af7b9bf
Sergey <vodopyanov03@bk.ru>
2024-05-19 16:15:32 +0300

    Поставил фронт админки, установил Admin LTE

--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,12 @@ Route::group(['namespace' => 'Main'], function () {
     Route::get('/', 'IndexController');
 });
 
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', 'IndexController');
+    });
+});
+
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
